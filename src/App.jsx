eaf063acb3bff8d6f344dd5b50977963552aa857
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-05 16:43:25 +0530

    Created the Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
 
     return (
         <>
-            <h1>Sasta YouTube</h1>
+            <Header />
 
             <Outlet />
         </>
