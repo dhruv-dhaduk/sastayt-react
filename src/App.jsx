e99a533845f67e35348633ae2e0f0b6b59864249
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-05 16:35:36 +0530

    Removed the testing button

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-import { paggedDataProvider } from './dataManager';
-const getNextVideos = paggedDataProvider();
-
 function App() {
-
-    async function handleClick() {
-        const data = await getNextVideos(5);
-        console.log(data);
-    }
 
     return (
         <>
             <h1>Sasta YouTube</h1>
-
-            <button
-                className='bg-white text-black m-4 p-4 border-2 border-white rounded-lg active:bg-black active:text-white'
-                onClick={handleClick}
-            >
-                Next Videos
-            </button>
         </>
     );
 }
