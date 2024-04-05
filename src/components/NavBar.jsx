da40faa48b55afa74bc70f3387a2e9bb8515bf53
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-05 18:32:48 +0530

    Created the NavItem component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
+import NavItem from "./NavItem";
+
 function NavBar() {
     return (
         <nav
             className="bg-black fixed border-gray-800 sm:border-t md:border-t-0 md:border-r sm:bottom-0 sm:inset-x-0 sm:w-full sm:h-12 md:left-0 md:top-14 md:w-16 md:h-full"
         >
-            
+            <NavItem />
         </nav>
     );
 }
---
+++ src/components/NavItem.jsx
@@ -0,0 +1,9 @@
+function NavItem() {
+    return (
+        <div>
+            NavItem
+        </div>
+    );
+}
+
+export default NavItem;
