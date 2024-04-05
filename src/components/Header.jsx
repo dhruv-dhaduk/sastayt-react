08d2e04fabd7a7a44bf560cab6864af57d2252ab
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-05 17:55:47 +0530

    Added active effects and link to logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,23 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
     return (
         <>
             <header
-                className="bg-slate-800 fixed top-0 inset-x-0 sm:h-12 md:h-14"
+                className="fixed top-0 inset-x-0 border-b border-b-gray-800 sm:h-12 md:h-14"
             >
-
-                <div
-                    className="h-full sm:p-3.5 md:p-4"
-                >
-                    <img 
-                        src="/logos/sastayt.png" 
-                        alt="Sasta YouTube logo" 
-                        className="h-full"
-                    />
-                </div>
+                
+                <Link to="/" >
+                    <div
+                        className="w-fit h-full active:bg-gray-800 sm:p-3.5 md:p-4"
+                    >
+                        <img 
+                            src="/logos/sastayt.png" 
+                            alt="Sasta YouTube logo" 
+                            className="h-full"
+                        />
+                    </div>
+                </Link>
                 
 
             </header>
