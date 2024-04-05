1453d97747c6138c4aaeaafac760b02764f5c3f7
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-05 23:37:01 +0530

    Added the button to test the data pipeline

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 
+import { paggedDataProvider } from './dataManager';
+const getNextVideos = paggedDataProvider();
+
 function App() {
+
+    async function handleClick() {
+        const data = await getNextVideos(5);
+        console.log(data);
+    }
 
     return (
         <>
@@ -10,6 +18,12 @@ function App() {
 
             <main className="p-2 sm:mt-12 md:mt-14 sm:mb-12 md:mb-0 md:ml-16" >
                 <Outlet />
+                <button 
+                    onClick={handleClick}
+                    className="bg-white text-black m-4 p-4 font-bold rounded-xl border-2 border-white active:bg-black active:text-white"
+                >
+                    Fetch Next Videos
+                </button>
             </main>
 
             <NavBar />
