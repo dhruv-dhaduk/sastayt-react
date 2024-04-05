3f20810e89f92bb5ad48d30af7f491512efd5982
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-05 19:06:11 +0530

    Used the props in NavItem component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,10 +5,10 @@ function NavBar() {
         <nav
             className="bg-black fixed border-gray-800 sm:border-t md:border-t-0 sm:flex md:block md:border-r sm:bottom-0 sm:inset-x-0 sm:w-full sm:h-12 md:left-0 md:top-14 md:w-16 md:h-full"
         >
-            <NavItem />
-            <NavItem />
-            <NavItem />
-            <NavItem />
+            <NavItem iconSrc="https://dhruv-dhaduk.github.io/assets/logos/white/home_filled_white.png" label="Home" />
+            <NavItem iconSrc="https://dhruv-dhaduk.github.io/assets/logos/white/refresh_white.png" label="Refresh" />
+            <NavItem iconSrc="https://dhruv-dhaduk.github.io/assets/logos/white/shuffle_white.png" label="Shuffle" />
+            <NavItem iconSrc="https://dhruv-dhaduk.github.io/assets/logos/white/back_to_top_white.png" label="Back To Top" />
         </nav>
     );
 }
--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,16 +1,16 @@
-function NavItem() {
+function NavItem({label, iconSrc}) {
     return (
         <div className="w-full h-full cursor-pointer flex flex-col justify-center items-center active:bg-gray-900 sm:flex-1 md:h-20">
             <img 
-                src="https://dhruv-dhaduk.github.io/assets/logos/white/home_filled_white.png"
-                alt="Home"
+                src={iconSrc}
+                alt={label}
                 draggable="false"
                 onContextMenu={e => e.preventDefault()}
                 className="select-none sm:w-5 md:w-6"
             />
 
             <p className="text-vsm select-none">
-                Home
+                {label}
             </p>
         </div>
     );
