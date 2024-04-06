6da6a14eb797a2a506d675d17d26fadbf8a2b3d0
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-06 17:15:57 +0530

    Scrolled to top when clicked on home btn

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,7 +14,7 @@ function NavBar({ shuffleFeed }) {
         <nav
             className="bg-black fixed z-40 border-gray-800 sm:border-t md:border-t-0 sm:flex md:block md:border-r sm:bottom-0 sm:inset-x-0 sm:w-full sm:h-12 md:left-0 md:top-14 md:w-16 md:h-full"
         >
-            <NavItem iconSrc={homeIcon} label="Home" onClick={() => navigate("/")} />
+            <NavItem iconSrc={homeIcon} label="Home" onClick={() => {navigate("/"); window.scrollTo(0, 0); }} />
             <NavItem iconSrc={refreshIcon} label="Refresh" onClick={() => window.location.reload()} />
             <NavItem iconSrc={shuffleIcon} label="Shuffle" onClick={shuffleFeed} />
             <NavItem iconSrc={backToTopIcon} label="Back To Top" onClick={() => window.scrollTo(0, 0)} />
