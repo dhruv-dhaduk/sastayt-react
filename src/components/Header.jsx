80b5913794e25d510f01af01ba74b981e7bc6ca8
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-06 15:16:22 +0530

    Added the z index to header and navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     return (
         <>
             <header
-                className="bg-black fixed top-0 inset-x-0 border-b border-b-gray-800 sm:h-12 md:h-14"
+                className="bg-black fixed top-0 inset-x-0 z-50 border-b border-b-gray-800 sm:h-12 md:h-14"
             >
                 
                 <Link to="/" >
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ import backToTopIcon from "/icons/backToTop.svg"
 function NavBar() {
     return (
         <nav
-            className="bg-black fixed border-gray-800 sm:border-t md:border-t-0 sm:flex md:block md:border-r sm:bottom-0 sm:inset-x-0 sm:w-full sm:h-12 md:left-0 md:top-14 md:w-16 md:h-full"
+            className="bg-black fixed z-40 border-gray-800 sm:border-t md:border-t-0 sm:flex md:block md:border-r sm:bottom-0 sm:inset-x-0 sm:w-full sm:h-12 md:left-0 md:top-14 md:w-16 md:h-full"
         >
             <NavItem iconSrc={homeIcon} label="Home" />
             <NavItem iconSrc={refreshIcon} label="Refresh" />
