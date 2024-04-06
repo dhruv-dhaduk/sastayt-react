72bfd9ff4471c944aec2b00381b3935947fd8e46
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-06 17:01:42 +0530

    Added the click listerner to refresh button in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ function NavBar() {
             className="bg-black fixed z-40 border-gray-800 sm:border-t md:border-t-0 sm:flex md:block md:border-r sm:bottom-0 sm:inset-x-0 sm:w-full sm:h-12 md:left-0 md:top-14 md:w-16 md:h-full"
         >
             <NavItem iconSrc={homeIcon} label="Home" />
-            <NavItem iconSrc={refreshIcon} label="Refresh" />
+            <NavItem iconSrc={refreshIcon} label="Refresh" onClick={() => window.location.reload()} />
             <NavItem iconSrc={shuffleIcon} label="Shuffle" />
             <NavItem iconSrc={backToTopIcon} label="Back To Top" onClick={() => window.scrollTo(0, 0)} />
         </nav>
