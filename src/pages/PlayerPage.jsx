c951f859219f54e51a2561eb6fe0a04155618095
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-06 23:08:31 +0530

    Added the video feed to player page

--- src/pages/PlayerPage.jsx
+++ src/pages/PlayerPage.jsx
@@ -1,13 +1,20 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { retriveVideosData } from '../dataManager';
 
+import Feed from '../components/Feed';
+import { VideoContext } from '../contexts/VideoContext.js';
+
 function PlayerPage() {
     const { id } = useParams();
+    const videoContext = useContext(VideoContext);
+
     const [video, setVideo] = useState({});
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
+        window.scrollTo(0, 0);
+
         retriveVideosData([id])
         .then((response) => {
             const data = response.find((item) => item.id === id);
@@ -19,8 +26,17 @@ function PlayerPage() {
     }, [id]);
     
     return (
-        <div>
-            { isLoading ? "Loading" : (video && video.id) ? JSON.stringify(video) : "Video Not Found" }
+        <div
+            className=""
+        >
+            <div>
+                { isLoading ? "Loading" : (video && video.id) ? JSON.stringify(video) : "Video Not Found" }
+            </div>
+
+            <div>
+                <Feed videos={videoContext.videos} />
+            </div>
+
         </div>
     );
 }
