2c180ea89c8083f8798d3baeda428e286504282f
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-07 21:46:26 +0530

    Set the positions and layout in VideoInfo

--- src/components/VideoInfo.jsx
+++ src/components/VideoInfo.jsx
@@ -9,44 +9,44 @@ import reportIcon from '/icons/report.svg';
 function VideoInfo({ video }) {
     return (
         <div>
+            <p className="line-clamp-3 sm:font-semibold md:font-bold sm:text-[17px] md:text-lg sm:px-4 md:px-0">
+                { video.title }
+            </p>
+            <p className="line-clamp-1 sm:text-[12px] md:text-sm text-stone-300 sm:px-4 md:px-0">
+                { convertUploadTimeFormat(video.uploadTime)}
+            </p>
 
-            <div className="sm:px-4 md:px-0">
-                <p className="line-clamp-3 sm:font-semibold md:font-bold sm:text-[17px] md:text-lg">
-                    { video.title }
-                </p>
-                <p className="line-clamp-1 sm:text-[12px] md:text-sm text-stone-300">
-                    { convertUploadTimeFormat(video.uploadTime)}
-                </p>
+            <div className="flex justify-between md:justify-start flex-wrap items-center py-3">
 
-                <div className="flex justify-between md:justify-start items-center py-3">
-                    <div className="flex justify-start items-center">
-                        <img 
-                            src={video.channelLogo}
-                            alt={video.channelTitle}
-                            draggable="false"
-                            onContextMenu={e => e.preventDefault()}
-                            onClick={e => { e.stopPropagation(); window.open(video.channelLink, "_blank") }}
-                            className="sm:w-10 sm:h-10 md:w-12 md:h-12 flex-none rounded-full"
-                        />
+                <div className="flex justify-start items-center sm:px-4 md:px-0">
+                    <img 
+                        src={video.channelLogo}
+                        alt={video.channelTitle}
+                        draggable="false"
+                        onContextMenu={e => e.preventDefault()}
+                        onClick={e => { e.stopPropagation(); window.open(video.channelLink, "_blank") }}
+                        className="sm:w-10 sm:h-10 md:w-12 md:h-12 flex-none rounded-full"
+                    />
 
-                        <p className="mx-4 font-semibold">
-                            { video.channelTitle }
-                        </p>
-                    </div>
+                    <p className="mx-4 font-semibold">
+                        { video.channelTitle }
+                    </p>
+                </div>
 
-                    <button className="bg-white text-black font-semibold rounded-full px-4 py-[6px]">
-                        Subscribe
-                    </button>
+                <button className="bg-white text-black font-semibold rounded-full px-4 py-[6px] md:mr-2">
+                    Subscribe
+                </button>
 
+                <div className="overflow-x-scroll py-4 no-scrollbar ml-auto sm:px-4 md:px-0">
                     <div className="flex">
-                        <Btn label="Like" icon={likeIcon} className="rounded-e-none mr-0 relative after:content-[''] after:border after:border-stone-400 after:h-[60%] after:absolute after:right-0"></Btn>
+                        <Btn label="Like" icon={likeIcon} className="rounded-e-none mx-0 relative after:content-[''] after:border after:border-stone-500 after:h-[60%] after:absolute after:right-0"></Btn>
                         <Btn label="" icon={dislikeIcon} className="rounded-s-none ml-0"></Btn>
                         <Btn label="Share" icon={shareIcon} className=""></Btn>
                         <Btn label="Save" icon={playlistIcon} className=""></Btn>
                         <Btn label="Report" icon={reportIcon} className=""></Btn>
                     </div>
-
                 </div>
+
             </div>
 
             <div className="border-t border-stone-700 px-4 py-3 text-sm md:hidden">
@@ -59,7 +59,7 @@ function VideoInfo({ video }) {
 function Btn({ label, icon, className }) {
     return (
         <button
-            className={`bg-stone-700 text-white rounded-full px-4 py-[6px] mx-1 flex items-center ${className}`}
+            className={`bg-stone-800 text-white rounded-full px-4 py-[6px] mx-1 flex-none flex items-center ${className}`}
         >
             <img 
                 src={icon}
