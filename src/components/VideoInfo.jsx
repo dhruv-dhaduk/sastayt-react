b56c6c4ce4b4bdc2419dde3f4f1bc61c5d9e6e32
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-07 20:40:58 +0530

    Made some changes to styling in VideoInfo

--- src/components/VideoInfo.jsx
+++ src/components/VideoInfo.jsx
@@ -5,14 +5,14 @@ function VideoInfo({ video }) {
         <div>
 
             <div className="sm:px-4 md:px-0">
-                <p className="line-clamp-3 sm:font-semibold md:font-bold md:text-lg">
+                <p className="line-clamp-3 sm:font-semibold md:font-bold sm:text-[17px] md:text-lg">
                     { video.title }
                 </p>
-                <p className="line-clamp-1 text-sm text-stone-300">
+                <p className="line-clamp-1 sm:text-[12px] md:text-sm text-stone-300">
                     { convertUploadTimeFormat(video.uploadTime)}
                 </p>
 
-                <div className="flex justify-between md:justify-start items-center py-4">
+                <div className="flex justify-between md:justify-start items-center py-3">
                     <div className="flex justify-start items-center">
                         <img 
                             src={video.channelLogo}
@@ -23,7 +23,7 @@ function VideoInfo({ video }) {
                             className="sm:w-10 sm:h-10 md:w-12 md:h-12 flex-none rounded-full"
                         />
 
-                        <p className="mx-4">
+                        <p className="mx-4 font-semibold">
                             { video.channelTitle }
                         </p>
                     </div>
@@ -35,7 +35,7 @@ function VideoInfo({ video }) {
                 </div>
             </div>
 
-            <div className="border-t border-stone-700 px-4 py-2 text-sm md:hidden">
+            <div className="border-t border-stone-700 px-4 py-3 text-sm md:hidden">
                 Up Next
             </div>
         </div>
