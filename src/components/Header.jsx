f24d4a2a63d77726309a0cc941b91557ec16c7fa
dhruv-dhaduk <dhadukd44@gmail.com>
2024-04-08 17:56:25 +0530

    Added the link to github in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,37 @@
 import { Link } from 'react-router-dom';
 import isMobileDevice from '../utils/isMobileDevice.js';
 
+import codeIcon from '/icons/code.svg';
+
 function Header() {
     return (
         <>
             <header
-                className="bg-black fixed top-0 inset-x-0 z-50 border-b border-b-gray-800 sm:h-12 md:h-14"
+                className="bg-black fixed top-0 inset-x-0 z-50 flex justify-start items-center border-b border-b-gray-800 sm:h-12 md:h-14"
             >
                 
-                <Link to="/" >
-                    <div
-                        className={`w-fit h-full sm:p-3.5 md:p-4 active:bg-gray-800 ${isMobileDevice ? "" : "hover:bg-gray-900"}`}
-                    >
-                        <img 
-                            src="/logos/sastayt.png" 
-                            alt="Sasta YouTube logo" 
-                            className="h-full"
-                        />
-                    </div>
+                <Link 
+                    to="/" 
+                    className={`w-fit h-full sm:p-3.5 md:p-4 active:bg-gray-800 ${isMobileDevice ? "" : "hover:bg-gray-900"}`}
+                >
+                    <img 
+                        src="/logos/sastayt.png" 
+                        alt="Sasta YouTube logo" 
+                        className="h-full"
+                    />
                 </Link>
                 
+                <a 
+                    href="https://github.com/dhruv-dhaduk/sastayt-react"
+                    target="_blank"
+                    className={`w-fit h-full ml-auto sm:p-3.5 md:p-4 active:bg-gray-800 ${isMobileDevice ? "" : "hover:bg-gray-900"}`}
+                >
+                    <img 
+                        src={codeIcon}
+                        alt="Source Code"
+                        className="h-full"
+                    />
+                </a>
 
             </header>
         </>
